072ace19776e5c27db50bdd882662889719a6ce7
Kellen McCarvel <kellen.mccarvel@gmail.com>
2016-04-24 14:55:11 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoBlog::Application.config.secret_key_base = 'f7c76a3106f45689ea11227702a4ad2f93e7732b63e67129d0e86e25c79ca0286c13cfcecb480c884c1dcfcb5e0a1b4140ed48eb5b46f7fdc5ae6c790294dc27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VideoBlog::Application.config.session_store :cookie_store, key: '_video_blog_session'
